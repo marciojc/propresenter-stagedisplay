cae9f9f8a53b0b36e8c5e77fcab5325f57883f04
Márcio Coelho <468890+marciojc@users.noreply.github.com>
2022-04-11 16:50:30 +0100

    Add readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# ProPresenter stage display
+
+## Install
+
+Copy `config.example.js` to `config.js` and add your configs.
+
+To run with docker-compose:
+
+`docker compose up -d`
+
+## Usage
+
+Open `http://localhost:8080` and verify if the connection is established.
+Go to stage display mode, on the pro presenter app change the slide, and the text will appear on the browser.
+
+
--- propresenter.js
+++ propresenter.js
@@ -4,49 +4,49 @@ var currentDiv = document.getElementById("current");
 var nextDiv = document.getElementById("next");
 
 function setCurrent(slide) {
-    if (currentDiv) {
-       slideNew = "";
+  if (currentDiv) {
+    slideNew = "";
 
-       slide = replaceNewLines(slide);
+    slide = replaceNewLines(slide);
 
-       //split
-       slideSplitted = slide.split("\n");
+    //split
+    slideSplitted = slide.split("\n");
 
-       slideSplitted.forEach(function(element) {
-           if (!element.startsWith(config['filter_lines_starts_with'])) {
-               slideNew += element + "\n";
-           }
-       });
+    slideSplitted.forEach(function (element) {
+      if (!element.startsWith(config['filter_lines_starts_with'])) {
+        slideNew += element + "\n";
+      }
+    });
 
-       slideHtml = slideNew.trim().replace(/\n/g, "<br />");
-       currentDiv.innerHTML = slideHtml;
-    }
+    slideHtml = slideNew.trim().replace(/\n/g, "<br />");
+    currentDiv.innerHTML = slideHtml;
+  }
 }
 
 function setNext(slide) {
-    if (nextDiv) {
-       slideNew = "";
+  if (nextDiv) {
+    slideNew = "";
 
-       slide = replaceNewLines(slide);
+    slide = replaceNewLines(slide);
 
-       //split
-       slideSplitted = slide.split("\n");
+    //split
+    slideSplitted = slide.split("\n");
 
-       slideSplitted.forEach(function(element) {
-           if (!element.startsWith(config['filter_lines_starts_with'])) {
-               slideNew += element + "\n";
-           }
-       });
+    slideSplitted.forEach(function (element) {
+      if (!element.startsWith(config['filter_lines_starts_with'])) {
+        slideNew += element + "\n";
+      }
+    });
 
-       slideHtml = slideNew.trim().replace(/\n/g, "<br />");
-       nextDiv.innerHTML = slideHtml;
-    }
+    slideHtml = slideNew.trim().replace(/\n/g, "<br />");
+    nextDiv.innerHTML = slideHtml;
+  }
 }
 
 function replaceNewLines(txt) {
-    //replace all different new line types
-    txt = txt.replace(/(?:\r\n|\r|\n|\u2028)/g, '\n');
-    return txt;
+  //replace all different new line types
+  txt = txt.replace(/(?:\r\n|\r|\n|\u2028)/g, '\n');
+  return txt;
 }
 
 function processMessage(message) {
@@ -58,7 +58,7 @@ function processMessage(message) {
       console.log("Authentication failed: " + message.err);
     }
   } else if (message.acn == "fv") {
-    for(a in message.ary) {
+    for (a in message.ary) {
       if (message.ary[a].acn == "cs") {
         setCurrent(message.ary[a].txt);
       } else if (message.ary[a].acn == "ns") {
@@ -69,34 +69,38 @@ function processMessage(message) {
 }
 
 function connect() {
-    var pp = 'ws://' + config['propresenter_ip'] + ':' + config['propresenter_port'] + '/stagedisplay';
-    
-    var ws = new WebSocket(pp);
+  var pp = 'ws://' + config['propresenter_ip'] + ':' + config['propresenter_port'] + '/stagedisplay';
 
-    ws.onopen = function() {
-        console.log('Connection established');
-        ws.send(JSON.stringify({"pwd":config['propresenter_password'],"ptl":610,"acn":"ath"}));
-    };
+  var ws = new WebSocket(pp);
 
-    ws.onmessage = function(e) {
-        processMessage(JSON.parse(e.data));
-    };
+  ws.onopen = function () {
+    console.log('Connection established');
+    ws.send(JSON.stringify({
+      "pwd": config['propresenter_password'],
+      "ptl": 610,
+      "acn": "ath"
+    }));
+  };
 
-    ws.onclose = function(e) {
-        console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
-        setCurrent("Waiting for data ...");
-        setNext("Waiting for data ...");
-        setTimeout(function() {
-            connect();
-        }, 1000);
-    };
+  ws.onmessage = function (e) {
+    processMessage(JSON.parse(e.data));
+  };
 
-    ws.onerror = function(err) {
-        console.error('Socket encountered error: ', err.message, 'Closing socket');
-        setCurrent("Waiting for data ...");
-        setNext("Waiting for data ...");
-        ws.close();
-    };
+  ws.onclose = function (e) {
+    console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
+    setCurrent("Waiting for data ...");
+    setNext("Waiting for data ...");
+    setTimeout(function () {
+      connect();
+    }, 1000);
+  };
+
+  ws.onerror = function (err) {
+    console.error('Socket encountered error: ', err.message, 'Closing socket');
+    setCurrent("Waiting for data ...");
+    setNext("Waiting for data ...");
+    ws.close();
+  };
 }
 
 connect();
